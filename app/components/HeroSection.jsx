eb5919e9a1b0b8618447bc71283d64a80ecd1643
JadenCCW <goku7174@gmail.com>
2024-07-01 14:50:29 -0700

    Hero Section Init

---
+++ app/components/HeroSection.jsx
@@ -0,0 +1,36 @@
+import React from 'react';
+import Image from "next/image";
+
+const HeroSection = () => {
+  return (
+    <section>
+        <div className="grid grid-cols-1 sm:grid-cols-12">
+            <div className="col-span-7 place-self-center text-center sm:text-left">
+                <h1 className="text-white mb-4 text-4xl sm:text-5xl lg:text-6xl font-extrabold">
+                    <span>hi im jaden</span>
+                </h1>
+                <p className="text-[#ADB7BE] text-base sm:text-lg mb-6 lg:text-xl">
+                    yadabababdboo
+                </p>
+                <div>
+
+                </div>
+
+            </div>
+            <div className="col-span-5 place-self-center mt-4 lg:mt-0">
+                <div className="rounded-full bg-[#181818] w-[250px] h-[250px] lg:w-[300px] lg:h-[300px] relative">
+                    <Image
+                        src="/images/jaden.jpg"
+                        alt="jaden image"
+                        className="absolute transform rounded-full aspect-square object-cover -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2"
+                        width={300}
+                        height={300}
+                    />
+                </div>
+            </div>
+        </div>
+    </section>
+  )
+}
+
+export default HeroSection
